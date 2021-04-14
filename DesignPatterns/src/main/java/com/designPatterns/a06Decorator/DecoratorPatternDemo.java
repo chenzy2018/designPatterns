179bb77910a2459eb9510c67a20c1793f0ecd8e2
chenzy2018 <cz773367227@163.com>
2021-04-14 17:20:35 +0800

    装饰器模式

--- DesignPatterns/src/main/java/com/designPatterns/a06Decorator/DecoratorPatternDemo.java
+++ DesignPatterns/src/main/java/com/designPatterns/a06Decorator/DecoratorPatternDemo.java
@@ -4,6 +4,9 @@ package com.designPatterns.a06Decorator;
 import com.designPatterns.Rectangle;
 import com.designPatterns.Shape;
 
+/**
+ * 装饰器模式
+ */
 public class DecoratorPatternDemo {
     public static void main(String[] args) {
 
@@ -15,5 +18,41 @@ public class DecoratorPatternDemo {
 
         System.out.println("\nRectangle of red border");
         redRectangle.draw();
+    }
+}
+
+/**
+ * 图形颜色装饰器
+ */
+abstract class ShapeDecorator implements Shape {
+
+    protected Shape decoratedShape;
+
+    public ShapeDecorator(Shape decoratedShape){
+        this.decoratedShape = decoratedShape;
+    }
+
+    public void draw(){
+        decoratedShape.draw();
+    }
+}
+
+/**
+ * 图形颜色装饰器具体实现
+ */
+class RedShapeDecorator extends ShapeDecorator{
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+    }
+
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setColor(decoratedShape);
+    }
+
+    void setColor(Shape decoratedShape) {
+        System.out.println("Border Color: Red");
     }
 }

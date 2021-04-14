8a405ca68d6d1a738a8bfdb6124ebc0e0663b2b2
chenzy2018 <cz773367227@163.com>
2021-04-14 17:17:49 +0800

    中介者模式

--- DesignPatterns/src/main/java/com/designPatterns/a05Mediator/MediatorPatternDemo.java
+++ DesignPatterns/src/main/java/com/designPatterns/a05Mediator/MediatorPatternDemo.java
@@ -2,6 +2,11 @@ package com.designPatterns.a05Mediator;
 
 import com.designPatterns.User;
 
+import java.util.Date;
+
+/**
+ * 中介者模式：mq就是典型案例，将网状变成星状
+ */
 public class MediatorPatternDemo {
     public static void main(String[] args) {
         User robert = new User("Robert");
@@ -9,5 +14,16 @@ public class MediatorPatternDemo {
 
         robert.sendMessage("Hi! John!");
         john.sendMessage("Hello! Robert!");
+    }
+}
+
+/**
+ * 中介类
+ */
+class ChatRoom {
+
+    public static void showMessage(User user, String message){
+        System.out.println(new Date().toString()
+                + " [" + user.getName() +"] : " + message);
     }
 }

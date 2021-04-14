d80a341be8f78bb63c8864e40a782ab779eb5ec6
chenzy2018 <cz773367227@163.com>
2021-04-14 17:12:29 +0800

    外观模式

--- DesignPatterns/src/main/java/com/designPatterns/a04Facade/FacadePatternDemo.java
+++ DesignPatterns/src/main/java/com/designPatterns/a04Facade/FacadePatternDemo.java
@@ -1,5 +1,10 @@
 package com.designPatterns.a04Facade;
 
+import com.designPatterns.Circle;
+import com.designPatterns.Rectangle;
+import com.designPatterns.Shape;
+import com.designPatterns.Square;
+
 /**
  * 只需要直到ShapeMaker类和对应API，对具体内部的Rectangle等类不需要关注
  */
@@ -12,3 +17,26 @@ public class FacadePatternDemo {
         shapeMaker.drawSquare();
     }
 }
+
+class ShapeMaker {
+    private Shape circle;
+    private Shape rectangle;
+    private Shape square;
+
+    public ShapeMaker() {
+        circle = new Circle();
+        rectangle = new Rectangle();
+        square = new Square();
+    }
+
+    public void drawCircle(){
+        circle.draw();
+    }
+    public void drawRectangle(){
+        rectangle.draw();
+    }
+    public void drawSquare(){
+        square.draw();
+    }
+}
+
